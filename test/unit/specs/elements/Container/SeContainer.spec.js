723c024025b57f6d200b57149330e15dac4479aa
Gahan Rakholia <gahan.rakholia@jda.com>
2018-04-13 02:25:54 +0530

    Resolves #1 Added UI ut's for Container

---
+++ test/unit/specs/elements/Container/SeContainer.spec.js
@@ -0,0 +1,38 @@
+import SeContainer from '@/elements/Container/SeContainer'
+import { mount } from '@vue/test-utils'
+
+const contentWrapper = {
+  render (h) {
+    return (
+      <div> Hello World </div>
+    )
+  }
+}
+
+describe('SeContainer', () => {
+  it('should have margins as per text attr', () => {
+    const data = { propsData: { text: true }, slots: { default: contentWrapper } }
+    const wrapper = mount(SeContainer, data)
+    expect(wrapper.contains('.text')).toBeTruthy()
+    expect(wrapper).toMatchSnapshot()
+  })
+
+  it('should align properly', () => {
+    const data = { propsData: { align: 'left' }, slots: { default: contentWrapper } }
+    const wrapper = mount(SeContainer, data)
+    expect(wrapper.classes()).toContain('left')
+    expect(wrapper.classes()).toContain('aligned')
+  })
+
+  it('should contiain class for justified', () => {
+    const data = { propsData: { justified: true }, slots: { default: contentWrapper } }
+    const wrapper = mount(SeContainer, data)
+    expect(wrapper.classes()).toContain('justified')
+  })
+
+  it('should contiain class for fluid', () => {
+    const data = { propsData: { fluid: true }, slots: { default: contentWrapper } }
+    const wrapper = mount(SeContainer, data)
+    expect(wrapper.classes()).toContain('fluid')
+  })
+})
